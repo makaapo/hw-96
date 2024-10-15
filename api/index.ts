6b198d18c911806476d1f956afd855a60ffd5be4
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-10-16 00:01:37 +0600

    api init app

---
+++ api/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import config from './config';
+import cors from 'cors';
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use(cors(config.corsOptions));
+app.use(express.static('public'));
+
+const run = async () => {
+  await mongoose.connect(config.database);
+
+  app.listen(port, () => {
+    console.log(`Server started on ${port} port!`);
+  });
+
+  process.on('exit', () => {
+    mongoose.disconnect();
+  });
+};
+
+run().catch(console.error);
